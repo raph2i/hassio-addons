69ad3f114fd4dfb48bd25a5c0b2a77e4565f4a0a
Raphael Pertl <raphael@pertl.tech>
2020-02-23 23:04:38 +0100

    update snapcast, so that it is really working \n sorry for pushing not working stuff :(

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -1,10 +1,39 @@
-#!/bin/bash
-
-CONFIG_PATH=/data/options.json
-
-SNAPSERVER_OPTS=$(jq --raw-output ".snapserveropts" $CONFIG_PATH)
+#!/usr/bin/env bashio
 
 mkdir -p /share/snapfifo
 
-echo "Start Snapserver..."
-/usr/bin/snapserver ${SNAPSERVER_OPTS}
+config=/etc/snapserver.conf
+
+if ! bashio::fs.file_exists '/etc/snapserver.conf'; then
+    touch /etc/snapserver.conf ||
+        bashio::exit.nok "Could not create snapserver.conf file on filesystem"
+fi
+bashio::log.info "Populating snapserver.conf..."
+
+# Start creation of configuration
+
+echo "[stream]" > "${config}"
+for stream in $(bashio::config 'stream.streams'); do
+    echo "stream = ${stream}" >> "${config}"
+done
+echo "buffer = $(bashio::config 'stream.buffer')" >> "${config}"
+echo "codec = $(bashio::config 'stream.codec')" >> "${config}"
+echo "send_to_muted = $(bashio::config 'stream.send_to_muted')" >> "${config}"
+echo "sampleformat = $(bashio::config 'stream.sampleformat')" >> "${config}"
+
+echo "[http]" >> "${config}"
+echo "enabled = $(bashio::config 'http.enabled')" >> "${config}"
+echo "doc_root = $(bashio::config 'http.docroot')" >> "${config}"
+
+echo "[tcp]" >> "${config}"
+echo "enabled = $(bashio::config 'tcp.enabled')" >> "${config}"
+
+echo "[logging]" >> "${config}"
+echo "debug = $(bashio::config 'logging.enabled')" >> "${config}"
+
+echo "[server]" >> "${config}"
+echo "threads = $(bashio::config 'server.threads')" >> "${config}"
+
+bashio::log.info "Starting SnapServer..."
+
+/usr/bin/snapserver -c /etc/snapserver.conf
